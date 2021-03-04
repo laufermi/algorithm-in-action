c00c4fdd242099485a350ed27f829f302f5f80a6
Zhao Liu <zhliu@thoughtworks.com>
2021-03-04 22:11:05 +0800

    Enhance selection sort

--- src/me/lauby/SelectionSort.java
+++ src/me/lauby/SelectionSort.java
@@ -3,6 +3,7 @@ package me.lauby;
 import me.lauby.utils.ArrayGenerator;
 import me.lauby.utils.Console;
 import me.lauby.utils.TestHelper;
+import me.lauby.utils.TestHelper.SortType;
 
 import java.time.Duration;
 import java.time.Instant;
@@ -12,28 +13,58 @@ public class SelectionSort {
 
     public static <E extends Comparable<E>> void sort(E[] arr) {
         for (int i = 0; i < arr.length; i++) {
+            int maxIdx = i;
             for (int j = i + 1; j < arr.length; j++) {
-                if (arr[j].compareTo(arr[i]) < 0) {
-                    swap(arr, i, j);
+                if (arr[j].compareTo(arr[maxIdx]) < 0) {
+                    maxIdx = j;
                 }
             }
+            swap(arr, i, maxIdx);
         }
     }
 
-    private static <E> void swap(E[] arr, int a, int b) {
-        E tmp = arr[a];
-        arr[a] = arr[b];
-        arr[b] = tmp;
+    public static <E extends Comparable<E>> void reverseSort(E[] arr) {
+        for (int i = arr.length - 1; i >= 0; i--) {
+            int maxIdx = i;
+            for (int j = i - 1; j >= 0; j--) {
+                if (arr[j].compareTo(arr[maxIdx]) > 0) {
+                    maxIdx = j;
+                }
+            }
+            swap(arr, i, maxIdx);
+        }
+    }
+
+    public static <E extends Comparable<E>> void sort(E[] arr, SortType type) {
+        for (int i = 0; i < arr.length; i++) {
+            int maxIdx = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (SortType.ASC == type && arr[j].compareTo(arr[maxIdx]) < 0) {
+                    maxIdx = j;
+                }
+                if (SortType.DESC == type && arr[j].compareTo(arr[maxIdx]) > 0) {
+                    maxIdx = j;
+                }
+            }
+            swap(arr, i, maxIdx);
+        }
+    }
+
+    private static <E> void swap(E[] arr, int from, int to) {
+        E tmp = arr[from];
+        arr[from] = arr[to];
+        arr[to] = tmp;
     }
 
     public static void main(String[] args) {
-        int[] lens = new int[]{1000, 10000};
+//        int[] lens = new int[]{1000, 10000};
+        int[] lens = new int[]{10};
         for (int len : lens) {
             Integer[] arr = ArrayGenerator.getRandomArray(len, len);
             Instant startTime = Instant.now();
-            SelectionSort.sort(arr);
+            SelectionSort.sort(arr, SortType.DESC);
             Instant endTime = Instant.now();
-            if (!TestHelper.isSortedArray(arr)) {
+            if (!TestHelper.isSortedArray(arr, SortType.DESC)) {
                 Console.log("[ERROR] Sort failed.");
                 for (int i : arr) {
                     Console.log("{} ", i);
--- src/me/lauby/utils/TestHelper.java
+++ src/me/lauby/utils/TestHelper.java
@@ -4,13 +4,18 @@ public class TestHelper {
 
     private TestHelper() {}
 
-    public static <E extends Comparable<E>> boolean isSortedArray(E[] arr) {
+    public static <E extends Comparable<E>> boolean isSortedArray(E[] arr, SortType sortType) {
         for (int i = 0; i < arr.length - 1; i++) {
-            if (arr[i].compareTo(arr[i + 1]) > 0) {
+            if (SortType.ASC == sortType && arr[i].compareTo(arr[i + 1]) > 0) {
                 return false;
             }
         }
         return true;
+    }
+
+    public enum SortType {
+        ASC,
+        DESC
     }
 
 }
